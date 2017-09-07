152e5ed5457d97f48509a7f10aceb03e89166f97
Ross Reicks <ross@Rosss-MacBook-Pro.local>
2017-09-06 21:07:32 -0500

    tried platform ready

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 
 @Component({
@@ -11,10 +11,14 @@ export class HomePage {
   private isBluetooth: boolean = false;
 
   private devices: any = [];
-  constructor(public navCtrl: NavController, bluetooth: BluetoothSerial) {
-    bluetooth.isEnabled().then(blue => this.isBluetooth = blue);
-
-    bluetooth.discoverUnpaired().then(res => this.devices = res);
+  constructor(public navCtrl: NavController,
+    private bluetooth: BluetoothSerial,
+    private platform: Platform) {
+    platform.ready().then(() => {
+        bluetooth.isEnabled().then(blue => this.isBluetooth = blue);
+      
+        bluetooth.discoverUnpaired().then(res => this.devices = res);
+    })
   }
 
 }
